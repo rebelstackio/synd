280a198aa69faa8e75a0c4a08aebd3c155678124
Javier Galarza <jegj57@gmail.com>
2019-11-05 16:40:08 -0500

    Refactor config object and added additional tests

--- src/lib/config/index.js
+++ src/lib/config/index.js
@@ -2,20 +2,39 @@
 'use strict';
 
 const DEFAULTS = require('./default.json');
+const os = require('os');
+const pathmodule = require('path');
 
 const Config = {
-	init({ configfile = DEFAULTS.conf_file, loadfile = require }={}) {
-		var ready = true;
-		var err = null;
+	ready: false,
+	cfg: null,
+	env: null,
+
+	init({ configfile = DEFAULTS.conf_file, loadfile = require, env = process.env.NODE_ENV, path = pathmodule }={}) {
 		try {
-			this.cfg = loadfile(configfile);
+			this.cfg = Object.assign(
+				{},
+				{ 'hostname' : os.hostname() },
+				DEFAULTS,
+				loadfile(path.resolve(this.configfile)),
+			);
+			this.configfile = configfile;
+			this.env = env;
 		} catch (error) {
-			ready = false;
-			err = error;
+			this.ready = false;
+			this.err = error;
 		} finally {
 			return {
-				getError: () => err,
-				isReady: () => ready
+				getError: () => this.err,
+				isReady: () => this.ready,
+				getConfigFile: () => this.configfile,
+				getEnv: () => this.env,
+				get: ( key ) => {
+					return this.cfg.hasOwnProperty(key) ? this.cfg[key] : undefined;
+				},
+				has: ( key ) => {
+					return this.cfg.hasOwnProperty(key) && Boolean(this.cfg[key])
+				}
 			};
 		}
 	}
--- test/src/lib/config/index.test.js
+++ test/src/lib/config/index.test.js
@@ -1,42 +1,68 @@
 /* test/src/lib/config/index.spec.js */
 'use strict';
 
+const os = require('os');
+
 const DEFAULTS = require('../../../../src/lib/config/default.json');
 const Config = require('../../../../src/lib/config');
 
+const loadfileMockOK = jest.fn(() => { });
+
+const pathMock = {
+	resolve: (s) => s
+};
+
 describe('Config Test Suit', () => {
 
-	test('init returns an object with the function isReady ', () => {
-		const config = Config.init();
-		expect(config).toHaveProperty('isReady');
+	describe('getters', () => {
+
+		test('isReady returns false if there was a problem loading the configuration', () => {
+			const loadfileMockFail = ( ) => {
+				throw new Error(`Unable to read config file`);
+			}
+			const config = Config.init({ loadfile: loadfileMockFail, path: pathMock });
+			expect(config).toHaveProperty('isReady');
+			expect(config.isReady()).toBeFalsy();
+		});
+
+		test('getError returns the last error loading the configuration', () => {
+			const loadfileMockFail = ( ) => {
+				throw new Error(`Unable to read config file`);
+			}
+			const config = Config.init({ loadfile: loadfileMockFail, path : pathMock });
+			expect(config).toHaveProperty('getError');
+			expect(config.getError()).toBeInstanceOf(Error);
+		});
+
+		test('getConfigFile returns the property configfile with default based on file src/lib/config/default.json', ()  => {
+			const config = Config.init({ configfile: '/opt/random/syndb.json', loadfile: loadfileMockOK, env: 'production', path : pathMock });
+			expect(config.getConfigFile()).toBe('/opt/random/syndb.json');
+		});
+
+		test('getEnv returns the env values based on the argument provided in the init', ()  => {
+			const config = Config.init({ loadfile: loadfileMockOK, env: 'production', path : pathMock });
+			expect(config.getEnv()).toBe('production');
+		});
+
 	});
 
-	test('init returns an object with the function getError', () => {
-		const config = Config.init();
-		expect(config).toHaveProperty('getError');
+	describe('Default values', () => {
+
+		test('Default value for hostname property is equal to host hostname value', () => {
+			const config = Config.init({ loadfile: loadfileMockOK, env: 'production', path : pathMock });
+			const hostname = os.hostname();
+			expect(config.get('hostname')).toBe(hostname);
+		});
+
+		test('Default value for conf_file property equal to /etc/watchtower/wt_conf.json', () => {
+			const config = Config.init({ loadfile: loadfileMockOK, env: 'production', path : pathMock });
+			expect(config.get('conf_file')).toBe(DEFAULTS.conf_file);
+		});
+
 	});
 
-	test('isReady returns false if there was a problem loading the configuration', () => {
-		const loadfileMockFail = ( ) => {
-			throw new Error(`Unable to read config file`);
-		}
-		const config = Config.init({ loadfile: loadfileMockFail });
-		expect(config.isReady()).toBeFalsy();
+	describe('Overwrite values with a real testing config file', () => {
+
 	});
 
-	test('getError returns the last error loding the configuration', () => {
-		const loadfileMockFail = ( ) => {
-			throw new Error(`Unable to read config file`);
-		}
-		const config = Config.init({ loadfile: loadfileMockFail });
-		expect(config.getError()).toBeInstanceOf(Error);
-	});
-
-	test('configfile should be equal to the default value(default.json) if it was not provided', () => {
-		const loadfileMockFail = jest.fn();
-		Config.init({ loadfile: loadfileMockFail });
-		expect(loadfileMockFail).toHaveBeenCalledWith(DEFAULTS.conf_file)
-	});
-
-	// TODO: Define config properties
 });
